d848893b302ba52ced9a0b105dc9a2ea6d09f2a1
unknown <chima.okwuokei@pau.edu.ng>
2024-12-25 14:32:19 +0100

    iris

--- src/iris-flower-classification/scripts/run-feature-and-prediction-pipelines.sh
+++ src/iris-flower-classification/scripts/run-feature-and-prediction-pipelines.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-cd src/01-module
+cd src/iris-flower-classification
 
 jupyter nbconvert --to notebook --execute iris-feature-pipeline.ipynb
 jupyter nbconvert --to notebook --execute iris-batch-inference-pipeline.ipynb
