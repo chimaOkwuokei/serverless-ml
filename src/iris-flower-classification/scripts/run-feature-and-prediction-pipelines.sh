d17791f3a07ec61aed0840bfcf40e8c3e862f76b
unknown <chima.okwuokei@pau.edu.ng>
2024-12-30 18:45:56 +0100

    feat: updated the dataset to comprise of uuid

--- src/iris-flower-classification/scripts/run-feature-and-prediction-pipelines.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-cd src/iris-flower-classification
-
-jupyter nbconvert --to notebook --execute iris-feature-pipeline.ipynb
-jupyter nbconvert --to notebook --execute iris-batch-inference-pipeline.ipynb
-
